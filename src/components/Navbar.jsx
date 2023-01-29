911db23f381211387a2729cb3cdbb63ffc0521f7
Salmi Mustapha <mustapha.salmi08000@gmail.com>
2023-01-29 11:25:11 +0100

    navbar mobile finie

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,23 @@
 import { useState } from 'react'
 
 function Navbar() {
-
+  const [isOpen, setIsOpen] = useState(false)
 
   return <nav>
-          <div>LOGO</div>
-          <ul className='btn-navbar'>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#news">New</a></li>
-            <li><a href="#contact">Popular</a></li>
-            <li><a href="#contact">Trending</a></li>
-            <li><a href="#about">Categories</a></li>
-          </ul>
-
+          <div className='logo'>LOGO</div>
+          <div className='icons-navbar-mobile' onClick={() => {setIsOpen(!isOpen)}}>
+            <img src="https://img.icons8.com/ios/100/null/menu-rounded.png" hidden={isOpen ? true : false}/>
+            <img src="https://img.icons8.com/material-outlined/96/null/multiply--v1.png" hidden={isOpen ? false : true} />
+          </div>
+          <div className={isOpen ? 'btn-navbar open' : 'btn-navbar'}>
+            <ul>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#news">New</a></li>
+              <li><a href="#contact">Popular</a></li>
+              <li><a href="#contact">Trending</a></li>
+              <li><a href="#about">Categories</a></li>
+            </ul>
+          </div>
         </nav>;
 }
 
